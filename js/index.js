d6d5fd8c8772f7fb595263f80ccf190fe6816f71
Julia <lotusmoonlianar@gmail.com>
2021-06-26 01:22:34 +0300

    completed project

---
+++ js/index.js
@@ -0,0 +1,46 @@
+// Домашка #18 — Storage
+
+// 1. Собрать данные с формы при ее сабмите
+// 2. Сохранить эти данные в качестве объекта внутри localStorage (не забываем о преобразовании в json)
+// 3. Создать второй html файл, в котором списком (ul > li) вывести все те данные, которые пользователь ввел в форму ранее
+// 4. Если данных нет, то показать соответствующее сообщение
+
+const formInformation = document.getElementById('form');
+
+formInformation.addEventListener('submit' , function (event) {
+    event.preventDefault();
+
+    let userInformation = formInformation.elements;
+    let saveFormInformation = {};
+console.log(userInformation);
+    for ( let key of userInformation){
+        saveFormInformation[key.name] = key.value;
+    }
+
+    localStorage.setItem(
+        'saveFormInformation',
+        JSON.stringify(saveFormInformation)
+    );
+
+    window.open('saveFormInformation.html');
+})
+
+
+function convertingInformation(){
+
+    if(localStorage.saveFormInformation){
+
+        let userInformation = formInformation.elements;
+        let convertingUserInformation = JSON.parse(localStorage.saveFormInformation);
+
+        for( let key of userInformation) {
+            key.value = convertingUserInformation[key.name];
+        }
+
+    } else {
+        alert('Вы впервые посетили нас. Пожалуйста, заполните форму.');
+        console.log('Вы впервые посетили нас. Пожалуйста, заполните форму.');
+    }
+}
+
+convertingInformation();
--- js/saveFormInformation.js
+++ js/saveFormInformation.js
@@ -1,18 +1,35 @@
-let convertingUserInformation = JSON.parse(localStorage.saveFormInformation);
-console.log(convertingUserInformation);
 
-// let list = document.querySelector('.list');
-// let li = document.createElement('LI');
-// li.innerHTML =
-// list.prepend(li);
+function showUserInformation(){
 
+   const list = document.querySelector('.list');
+   let userInform = localStorage.getItem('saveFormInformation')
 
+   if (userInform){
 
-   for (let key in convertingUserInformation) {
-       if(convertingUserInformation.hasOwnProperty(key)){
-           console.log(`${key} : ${convertingUserInformation[key]}`)
+       let convertingUserInformation = JSON.parse(userInform);
+
+       for (let key in convertingUserInformation){
+
+           let li = document.createElement('LI');
+
+           if (convertingUserInformation[key]){
+               li.innerHTML = `${key} - ${convertingUserInformation[key]}`;
+               list.append(li);
+           }else {
+               li.innerHTML = `${key} - данные не заполнены`;
+               list.append(li);
+           }
        }
    }
+    else {
+       alert('Контактные данные не заполнены.');
+       console.log('Контактные данные не заполнены.');
+   }
+}
+showUserInformation();
 
-
-
+//костыль для удаления данных о кнопке submit
+function deleteButtonValue(){
+    document.querySelector('UL').lastChild.remove();
+}
+deleteButtonValue();
